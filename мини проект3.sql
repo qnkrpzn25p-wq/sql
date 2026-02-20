4dba04783e878265bf5fea25c8c77f84881e3890
Aygerim <qnkrpzn25p@privaterelay.appleid.com>
2026-02-20 05:20:39 +0500

    проект sql

--- мини проект3.sql
+++ мини проект3.sql
@@ -1,4 +1,36 @@
-	#Выведите сколько пользователей добавили книгу 'Coraline', сколько пользователей прослушало больше 10%. 
+-----------Oтделы, где средняя зарплата выше общей средней зарплаты по компании.
+SELECT dept_name 
+FROM departments d
+JOIN dept_emp de ON d.dept_no = de.dept_no
+JOIN salaries s ON  de.emp_no = s.emp_no
+GROUP BY dept_name
+HAVING AVG(salary) > (SELECT AVG(salary) FROM salaries);
+
+
+-----------Определение наивысшей текущей зарплаты в каждом отделе 
+SELECT de.emp_no, s.salary, de.dept_no, 
+	MAX(s.salary) OVER (PARTITION BY de.dept_no) AS max_salary_dept
+FROM dept_emp de JOIN salaries s ON de.emp_no = s.emp_no
+WHERE de.to_date = '9999-01-01'
+ORDER BY de.dept_no, s.salary DESC;
+
+
+-----------Ранжирование сотрудников в отделе по стажу работы:
+SELECT e.emp_no, de.dept_no, e.hire_date, 
+	DENSE_RANK() OVER (PARTITION BY de.dept_no ORDER BY e.hire_date ASC) AS experience_rank
+FROM employees e JOIN dept_emp de ON e.emp_no = de.emp_no
+WHERE de.to_date = '9999-01-01' 
+ORDER BY de.dept_no, experience_rank;
+
+
+-----------Определение начальной и последней зарплаты сотрудника:
+SELECT e.emp_no, s.salary, 
+	FIRST_VALUE(s.salary) OVER (PARTITION BY e.emp_no ORDER BY e.emp_no) AS first_salary,
+	LAST_VALUE(s.salary) OVER (PARTITION BY e.emp_no ORDER BY e.emp_no) AS last_salary
+FROM employees e JOIN salaries s ON e.emp_no = s.emp_no;
+
+
+-----------Количество пользователей добавивших книгу 'Coraline' и сколько пользователей прослушало больше 10%. 
 SELECT COUNT(DISTINCT ac.user_id) AS count_user,
 	(SELECT COUNT(DISTINCT ac1.user_id) AS count_user 
 	FROM audiobooks a1 
@@ -9,7 +41,8 @@ FROM audiobooks a
 JOIN audio_cards ac ON ac.audiobook_uuid = a.uuid
 WHERE a.title = 'Coraline';
 
-	#По каждой операционной системе и названию книги выведите количество пользователей, сумму прослушивания в часах, не учитывая тестовые прослушивания. 
+
+-----------Количество пользователей, сумма прослушивания в часах, не учитывая тестовые прослушивания по каждой книге. 
 SELECT l.os_name, a.title, 
 	COUNT(DISTINCT l.user_id) AS count_user, 
     SUM(l.position_to - l.position_from)/3600 AS sum_listen
@@ -18,7 +51,8 @@ JOIN listenings l ON l.audiobook_uuid = a.uuid
 WHERE l.is_test = 0
 GROUP BY l.os_name, a.title;
 
-	#Найдите книгу, которую слушает больше всего людей. 
+
+-----------Книга, которую слушает больше всего людей. 
 SELECT a.title, COUNT(*) AS listen
 FROM audio_cards ac 
 JOIN audiobooks a ON ac.audiobook_uuid = a.uuid
@@ -26,7 +60,8 @@ GROUP BY a.title
 ORDER BY listen DESC
 LIMIT 1;
     
-	#Найдите книгу, которую чаще всего дослушивают до конца.
+
+------------Книга, которую чаще всего дослушивают до конца.
 SELECT a.title, COUNT(*) AS count_finished
 FROM audio_cards ac 
 JOIN audiobooks a ON ac.audiobook_uuid = a.uuid
@@ -34,6 +69,10 @@ WHERE ac.state = 'finished'
 GROUP BY a.title
 ORDER BY count_finished DESC
 LIMIT 1;
+
+
+
+
 
 
 
